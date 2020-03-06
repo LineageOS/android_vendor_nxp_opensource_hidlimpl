a7589d8774808a64d1d2e8de9f5416faf10eedd9
Bhuvan Varshney <bvarshne@codeaurora.org>
2020-03-06 18:33:35 +0530

    NFC: HIDL implementation manual merge
    Manual merge for HAL service implementation files
    from third party release for MW upgradation to 11.01.00.
    
    Change-Id: I24765e5c08fafc39a7c2378bb2daf1f6ffffe4f2

--- 2.0/default/NqNfc.h
+++ 2.0/default/NqNfc.h
@@ -27,6 +27,26 @@
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+/*
+ * Not a contribution.
+ */
+
+/*
+ *  Copyright 2018-2020 NXP
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 #ifndef VENDOR_NXP_HARDWARE_NFC_V2_0_NQNFC_H
 #define VENDOR_NXP_HARDWARE_NFC_V2_0_NQNFC_H
 
@@ -34,6 +54,15 @@
 #include <vendor/nxp/hardware/nfc/2.0/types.h>
 #include <hidl/MQDescriptor.h>
 #include <hidl/Status.h>
+
+enum Constants : uint16_t {
+    HAL_NFC_ESE_HARD_RESET = 5,
+};
+
+enum NxpNfcEvents : uint8_t {
+    HAL_NFC_RESTART = 0x30,
+    HAL_NFC_HCI_RESET = 0x40,
+};
 
 namespace vendor {
 namespace nxp {
@@ -67,6 +96,15 @@ struct NqNfc : public INqNfc {
           getSystemProperty_cb _hidl_cb) override;
     Return<bool> setSystemProperty(const ::android::hardware::hidl_string& key,
           const ::android::hardware::hidl_string& value)  override;
+    Return<void> getVendorParam(const ::android::hardware::hidl_string &key,
+          getVendorParam_cb _hidl_cb) override;
+    Return<bool> setVendorParam(const ::android::hardware::hidl_string &key,
+          const ::android::hardware::hidl_string &value) override;
+    Return<bool> resetEse(uint64_t resetType) override;
+    Return<bool> setEseUpdateState(NxpNfcHalEseState state) override;
+    Return<bool> setNxpTransitConfig(const ::android::hardware::hidl_string &strval) override;
+    Return<bool> isJcopUpdateRequired() override;
+    Return<bool> isLsUpdateRequired() override;
 };
 
 }  // namespace implementation
